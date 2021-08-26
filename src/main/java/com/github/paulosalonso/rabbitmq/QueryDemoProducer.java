a8ff6594f8d2b97a5feb16b2f99903f15da7c0fe
Paulo Alonso <paulo.alonso@luizalabs.com>
2021-08-26 08:51:19 -0300

    Adding RabbitMQ container definitions

--- src/main/java/com/github/paulosalonso/rabbitmq/QueryDemoProducer.java
+++ src/main/java/com/github/paulosalonso/rabbitmq/QueryDemoProducer.java
@@ -1,22 +1,25 @@
 package com.github.paulosalonso.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class QueryDemoProducer {
 
-    protected static final String EXCHANGE = "paulosalonso";
-    protected static final String ROUTING_KEY = "demo";
+    protected static final String EXCHANGE = "demo-exchange";
+    protected static final String ROUTING_KEY = "demo-queue";
 
     private final RabbitTemplate rabbitTemplate;
 
     @PostConstruct
     public void publish() {
         rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY, new DemoDTO("Published by application producer"));
+        log.info("Message successfully published. Exchange: {}, Rounting Key: {}", EXCHANGE, ROUTING_KEY);
     }
 }
